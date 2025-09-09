22eaf3b3c39e4576dd10bf09d64b2292e0e35cb2
khaled ghaly <www.gmiza2967804@gmail.com>
2025-09-09 12:02:46 +0300

    Fix Spinner

--- js/main.js
+++ js/main.js
@@ -65,75 +65,75 @@
 })(jQuery);
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const languageButton = document.getElementById("languageButton");
-const elementsToTranslate = {
-    // Navigation
-    homeNav: { en: "Home", ar: "الرئيسية" },
-    aboutNav: { en: "About Us", ar: "عن الشركة" },
-    servicesNav: { en: "Services", ar: "الخدمات" },
-    projectsNav: { en: "Projects", ar: "المشاريع" },
-    otherPagesNav: { en: "Other Pages", ar: "صفحات أخرى" },
-    featuresNav: { en: "Features", ar: "المميزات" },
-    teamNav: { en: "Our Team", ar: "فريق العمل" },
-    testimonialsNav: { en: "Testimonials", ar: "آراء العملاء" },
-    error404Nav: { en: "404 Page", ar: "صفحة خطأ 404" },
-    contactNav: { en: "Contact Us", ar: "اتصل بنا" },
+// document.addEventListener("DOMContentLoaded", function () {
+//   const languageButton = document.getElementById("languageButton");
+// const elementsToTranslate = {
+//     // Navigation
+//     homeNav: { en: "Home", ar: "الرئيسية" },
+//     aboutNav: { en: "About Us", ar: "عن الشركة" },
+//     servicesNav: { en: "Services", ar: "الخدمات" },
+//     projectsNav: { en: "Projects", ar: "المشاريع" },
+//     otherPagesNav: { en: "Other Pages", ar: "صفحات أخرى" },
+//     featuresNav: { en: "Features", ar: "المميزات" },
+//     teamNav: { en: "Our Team", ar: "فريق العمل" },
+//     testimonialsNav: { en: "Testimonials", ar: "آراء العملاء" },
+//     error404Nav: { en: "404 Page", ar: "صفحة خطأ 404" },
+//     contactNav: { en: "Contact Us", ar: "اتصل بنا" },
 
-    // Spinner
-    loading: { en: "Loading...", ar: "جاري التحميل..." },
+//     // Spinner
+//     loading: { en: "Loading...", ar: "جاري التحميل..." },
 
-};
+// };
 
-  let currentLanguage = localStorage.getItem("language") || "ar";
+//   let currentLanguage = localStorage.getItem("language") || "ar";
 
-  function updateLanguage() {
-    for (const id in elementsToTranslate) {
-      const element = document.getElementById(id);
-      if (element) {
-        element.textContent = elementsToTranslate[id][currentLanguage];
-      }
-    }
+//   function updateLanguage() {
+//     for (const id in elementsToTranslate) {
+//       const element = document.getElementById(id);
+//       if (element) {
+//         element.textContent = elementsToTranslate[id][currentLanguage];
+//       }
+//     }
 
-    // تحديث لغة الصفحة واتجاه النص
-    document.documentElement.lang = currentLanguage === "en" ? "en" : "ar";
-    // document.style.direction = currentLanguage === "en" ? "ltr" : "rtl";
-    document.body.style.textAlign = currentLanguage === "en" ? "left" : "right";
-    if (languageButton) {
-      languageButton.textContent =
-        elementsToTranslate["languageButton"][currentLanguage];
-    }
-  }
+//     // تحديث لغة الصفحة واتجاه النص
+//     document.documentElement.lang = currentLanguage === "en" ? "en" : "ar";
+//     // document.style.direction = currentLanguage === "en" ? "ltr" : "rtl";
+//     document.body.style.textAlign = currentLanguage === "en" ? "left" : "right";
+//     if (languageButton) {
+//       languageButton.textContent =
+//         elementsToTranslate["languageButton"][currentLanguage];
+//     }
+//   }
 
-  languageButton.addEventListener("click", function () {
-    currentLanguage = currentLanguage === "en" ? "ar" : "en";
-    localStorage.setItem("language", currentLanguage);
-    updateLanguage();
-    console.log(localStorage.getItem("language"));
-  });
+//   languageButton.addEventListener("click", function () {
+//     currentLanguage = currentLanguage === "en" ? "ar" : "en";
+//     localStorage.setItem("language", currentLanguage);
+//     updateLanguage();
+//     console.log(localStorage.getItem("language"));
+//   });
 
-  updateLanguage(); // تطبيق اللغة المختارة عند تحميل الصفحة
-});
+//   updateLanguage(); // تطبيق اللغة المختارة عند تحميل الصفحة
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const footerAboutDiv = document.querySelector(".footer-about");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const footerAboutDiv = document.querySelector(".footer-about");
 
-  function isArabic() {
-    const htmlElement = document.querySelector("html");
-    return htmlElement.getAttribute("lang") === "ar";
-    // يمكنك استخدام طريقة أخرى للتحقق من اللغة إذا لزم الأمر
-    // return document.body.classList.contains('rtl');
-  }
+//   function isArabic() {
+//     const htmlElement = document.querySelector("html");
+//     return htmlElement.getAttribute("lang") === "ar";
+//     // يمكنك استخدام طريقة أخرى للتحقق من اللغة إذا لزم الأمر
+//     // return document.body.classList.contains('rtl');
+//   }
 
-  if (footerAboutDiv) {
-    if (isArabic()) {
-      footerAboutDiv.classList.remove("text-md-start");
-      footerAboutDiv.classList.add("text-md-end");
-    } else {
-      footerAboutDiv.classList.remove("text-md-end");
-      footerAboutDiv.classList.add("text-md-start");
-    }
-  }
-});
+//   if (footerAboutDiv) {
+//     if (isArabic()) {
+//       footerAboutDiv.classList.remove("text-md-start");
+//       footerAboutDiv.classList.add("text-md-end");
+//     } else {
+//       footerAboutDiv.classList.remove("text-md-end");
+//       footerAboutDiv.classList.add("text-md-start");
+//     }
+//   }
+// });
 
-document.getElementById("currentYear").textContent = new Date().getFullYear();
+// document.getElementById("currentYear").textContent = new Date().getFullYear();
